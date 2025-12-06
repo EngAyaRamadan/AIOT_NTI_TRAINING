44bc6ec92de54fd536b0d33150828dd16b32682e
Aya-Ramadan <ayaramadan@gmail.com>
2025-12-06 09:16:19 +0200

    update config file

--- Moduals_Driver/src/APP_cfg.h
+++ Moduals_Driver/src/APP_cfg.h
@@ -9,8 +9,8 @@
 //HAL Module definitions
 #define GPIO_ENABLED STD_ON
 #define SENSORH_ENABLED STD_ON
-
-
+#define UART_ENABLED STD_ON
+#define WIFI_ENABLED STD_ON
 // APP Module definitions
 #define LED_ENABLED STD_ON
 #define BUTTON_ENABLED STD_ON
@@ -21,8 +21,8 @@
 // HAL Debug Definitions
 #define GPIO_DEBUG STD_ON
 #define SENSORH_DEBUG STD_ON
-
-
+#define UART_DEBUG STD_ON
+#define WIFI_DEBUG STD_ON
 
 // APP Debug Definitions
 #define LED_DEBUG STD_ON
@@ -46,6 +46,8 @@
 
 // ---------------------Module Configration-----------------------------------------
 // HAL Configuration
+
+// ADC 
 #define SENSORH_RESOLUTION 12
 #define MIN_SENSORH_VALUE 0
 #define MAX_SENSORH_VALUE (pow(2, SENSORH_RESOLUTION) - 1)
@@ -59,6 +61,20 @@
 
 #define SOUND_SPEED_US 0.0346f
 #define US_ECHO_TIMEOUT_US 30000UL
+
+// UART Configuration
+#define UART_BUAD_RATE 115200
+#define UART_FRAMELENGTH SERIAL_8N1
+#define UART_TXPIN 17
+#define UART_RXPIN 16
+
+// wifi parameter
+#define WIFI_SSID "Aya Ramadan"
+#define WIFI_PASSWORD "ayaramadan22"
+#define TIMEOUT_MS 1500
+#define WIFI_RECONNECT_TIMEOUT 3
+
+
 
 // APP Configuration
 // Button
