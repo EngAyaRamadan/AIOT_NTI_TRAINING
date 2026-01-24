c0195ec32cb4552c2506f82e637be3a8e8319e2d
Aya-Ramadan <ayaramadan@gmail.com>
2026-01-24 14:52:37 +0200

    Rename Folder

--- Moduals_Driver/MQTT_HiveMq_Project/src/APP/POT/POT.h
+++
@@ -1,9 +0,0 @@
-#ifndef POT_H
-#define POT_H
-
-void POT_init(uint8_t POT_pin);
-float POT_main(uint8_t POT_pin);
-
-float POT_value_Getter(void);
-
-#endif
--- Moduals_Driver/src/APP/LM35/LM35.h
+++
@@ -1,9 +0,0 @@
-#ifndef LM35_H
-#define LM35_H
-#include <stdio.h>
-
-void LM35_Init(uint8_t LM35_pin);
-float LM35_main(uint8_t LM35_pin);
-float LM35_value_Getter(void);
-
-#endif
